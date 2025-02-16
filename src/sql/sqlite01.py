b1c1d2084c3881a25826ee500e616e3e5ac857d4
rosso0815 <rosso0815@gmail.com>
2025-02-16 07:42:12 +0100

    update

--- src/sql/sqlite01.py
+++ src/sql/sqlite01.py
@@ -3,7 +3,11 @@
 import sqlite3
 
 con = sqlite3.connect("all.db")
+
 new_cur = con.cursor()
+
 res = new_cur.execute("SELECT count(*) FROM picture")
+
 count = res.fetchone()
+
 print(f'count: x{count}x')
